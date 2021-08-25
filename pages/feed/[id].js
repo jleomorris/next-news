958c5c935d95cd20d128467720fc92d2f13a7e10
James Morris <james-morris2010@hotmail.co.uk>
2021-08-25 23:31:58 +0100

    Add Feed component

---
+++ pages/feed/[id].js
@@ -0,0 +1,58 @@
+import { addSyntheticLeadingComment } from "typescript";
+import feedStyles from "../../styles/Feed.module.css";
+
+const Feed = ({ pageNumber, articles }) => {
+  console.log(pageNumber, articles);
+
+  return (
+    <div className={feedStyles.main}>
+      {articles.map((article) => (
+        <div key={article.publishedAt} className={feedStyles.post}>
+          <h1 onClick={() => (window.location.href = article.url)}>
+            {" "}
+            {article.title}
+          </h1>
+          <p>{article.description}</p>
+          {!!article.urlToImage && <img src={article.urlToImage} />}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const pageNumber = pageContext.query.id;
+
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
+    return {
+      props: {
+        articles: [],
+        pageNumber: 1,
+      },
+    };
+  }
+
+  const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=gb&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    }
+  );
+
+  const apiJson = await apiResponse.json();
+
+  // console.log("feed.apiJson", apiJson);
+
+  const { articles } = apiJson;
+
+  return {
+    props: {
+      articles,
+      pageNumber: Number.parseInt(pageNumber),
+    },
+  };
+};
+
+export default Feed;
