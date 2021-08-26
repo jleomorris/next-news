b773a95fd29048b362a779de640c6d7ecdc4c337
James Morris <james-morris2010@hotmail.co.uk>
2021-08-26 10:43:46 +0100

    Add nav icon

--- components/Nav.js
+++ components/Nav.js
@@ -2,23 +2,28 @@ import { styles } from "ansi-colors";
 import { useRouter } from "next/router";
 // Styles
 import navStyles from "../styles/Nav.module.css";
+import Image from "next/image";
+import Newspaper from "../components/Newspaper";
 
 const Nav = () => {
   const router = useRouter();
 
   return (
     <div className={navStyles.main}>
+      <div className={navStyles.icon}>
+        <Newspaper />
+      </div>
       <div onClick={() => router.push("/")}>Home</div>
       <div onClick={() => router.push("/feed/1")}>Feed</div>
       <div onClick={() => router.push("/eom")}>EOM</div>
-      <div
+      {/* <div
         onClick={() =>
           (window.location.href =
             "https://www.linkedin.com/in/james-morris-62a66b166/")
         }
       >
         LinkedIn
-      </div>
+      </div> */}
     </div>
   );
 };
